0dead52031bdfddff31e3406004cf77af796c0b8
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-08-16 21:03:56 +0530

    Update imputer.py

--- Data Preprocessing/imputer.py
+++ Data Preprocessing/imputer.py
@@ -1,1 +1,17 @@
+# The code replaces missing data with appropriate info/data
 
+import pandas as pd
+
+from sklearn.preprocessing import Imputer
+
+dataSet = pd.read_csv('Data.csv')
+X = dataSet.iloc[:, :-1].values     #taking all the rows of all the columns except the last one!
+Y = dataSet.iloc[:, -1].values      #taking all the rows of the last column
+
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)    #axis=0 is columns
+X[:, 1:3] = imputer.fit_transform(X[:, 1:3])
+
+''' or
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+'''
