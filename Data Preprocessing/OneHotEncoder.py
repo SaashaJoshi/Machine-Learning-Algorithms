1c1fb1c2e8dfa071e083a0f85a7b3c87df054ff7
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-08-16 21:32:17 +0530

    Create OneHotEncoder.py

---
+++ Data Preprocessing/OneHotEncoder.py
@@ -0,0 +1,13 @@
+# Dummy encoding categorical data
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+dataSet = pd.read_csv('Data.csv')
+X = dataSet.iloc[:, :-1].values     #taking all the rows of all the columns except the last one!
+Y = dataSet.iloc[:, -1].values   
+
+labelX=LabelEncoder()
+X[:,0]=labelX.fit_transform(X[:,0])
+hotEncoder= OneHotEncoder(categorical_features=[0])
+X=hotEncoder.fit_transform(X).toarray()
