ae7b2cb1fc6859d7cef9bf1252da5db47cfb5e64
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-08-17 01:34:35 +0530

    Update template.py

--- Data Preprocessing/template.py
+++ Data Preprocessing/template.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Importing the Dataset
 dataSet = pd.read_csv('Data.csv')
 X = dataSet.iloc[:, :-1].values    
 Y = dataSet.iloc[:, -1].values   
 
-from sklearn.model_selection import train_test_split      # Splitting of data into training set and test set!
+# Splitting of data into training set and test set!
+from sklearn.model_selection import train_test_split      
 Xtrain, Xtest, Ytrain, Ytest= train_test_split(X, Y, test_size=0.2, random_state=0)
 
 '''
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
 scX=StandardScaler()
 Xtrain=scX.fit_transform(Xtrain)
