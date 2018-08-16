8c073caada53d5f1ce0e64f4e1338f6893ee8661
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-08-16 21:00:13 +0530

    Create LabelEncoder.py

---
+++ Data Processing/LabelEncoder.py
@@ -0,0 +1,11 @@
+# Encoding categorical data
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+dataSet = pd.read_csv('Data.csv')
+X = dataSet.iloc[:, :-1].values     #taking all the rows of all the columns except the last one!
+Y = dataSet.iloc[:, -1].values   
+
+labelX=LabelEncoder()
+X[:,0]=labelX.fit_transform(X[:,0])
