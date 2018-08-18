08589b9f47b89b33b427aa5feb43c8e0203c4a9d
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-08-18 20:47:52 +0530

    Create linearRegression.py

---
+++ Simple Linear Regression/linearRegression.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Importing the Dataset
+dataSet = pd.read_csv('Salary_Data.csv')
+X = dataSet.iloc[:, :-1].values
+y = dataSet.iloc[:, -1].values
+
+# Splitting of data into training set and test set!
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest= train_test_split(X, y, test_size=1/3, random_state=0)
+
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()        #instantiating the LinearRegression model
+regressor.fit(Xtrain, ytrain)       #fits moel on training data
